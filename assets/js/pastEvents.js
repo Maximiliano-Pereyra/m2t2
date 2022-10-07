4174c53fa98440e89104efba4a03deba4776c9b8
Maximiliano-Pereyra <maxigenio99@gmail.com>
2022-10-07 10:13:33 -0300

    past-events-js

---
+++ assets/js/pastEvents.js
@@ -0,0 +1,25 @@
+let cardContainer = document.getElementById ('cardContainer')
+
+for (let i = 0; i < data.events.length; i++){
+    if((data.currentDate) > (data.events[i].date)){
+        let divCard = document.createElement('div')
+        divCard.classname = 'card gap-2';
+        divCard.style.width = '19rem'
+        divCard.style.height = "25rem"
+        divCard.style.border = ' solid 1px black'
+        divCard.innerHTML = ` <div class="d-flex justify-content-between flex-wrap">
+    <img src="${data.events[i].image}" style="border-solid-dark" alt="..." width="280px" height="200px" </img>
+                                   <h5 class="card-title"> ${data.events[i].name}</h5>
+                                   <p class="card-text"> ${data.events[i].date}</p>
+                                   <p class="card-text"> ${data.events[i].description}</p>
+                                   <p>price: ${data.events[i].price}</p>
+                                   <a href="./details.html" class="btn btn-primary btnCard">Buy</a>
+                                   </div>
+                                   `
+                                   
+                                cardContainer.appendChild(divCard)
+    }
+}
+
+console.log(typeof parseInt(data.events[0].date))
+console.log(typeof parseInt(data.currentDate))
